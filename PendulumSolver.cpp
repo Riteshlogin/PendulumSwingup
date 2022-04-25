e2914eb0b3c273ecc79b8d6cbd7b68d658163311
Ritesh Misra <riteshlogin@gmail.com>
2022-04-25 01:54:51 -0700

    Value iteration seems to be working sometimes, need to improve convergence condition

--- PendulumSolver.cpp
+++ PendulumSolver.cpp
@@ -2,8 +2,14 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
-
 #include "GridCell.hpp"
+
+#define DT 0.01
+
+void inverseStateTransition(double angle, double angular_velocity, double &torque, double new_angle, double new_angular_velocity);
+void stateTransition(double angle, double angular_velocity, double torque, double &new_angle, double &new_angular_velocity);
+int bellman_update(std::vector<std::vector<GridCell>> &state_mat);
+void constrain(int &value, int min, int max);
 
 int main(){
 
@@ -17,24 +23,33 @@ std::vector <std::vector<GridCell>> state_mat (2000, std::vector<GridCell>(2000)
 state_mat[0][1000].value = 50;
 state_mat[1999][1000].value = 50;
 
+int x = bellman_update(state_mat);
 
 
-std::cout << "Hello World" << std::endl;
+std::cout << state_mat[5][950].value << std::endl;
 
 }
 
 
-int bellman_update(std::vector<std::vector<GridCell>> state_mat){
+int bellman_update(std::vector<std::vector<GridCell>> &state_mat){
 
-    for (int i = 0; i < 2000; i++){
-        for (int j = 0; j < 2000; j++){
-            
-            // Calculate the optimal torque for the current state.
-            double new_angular_velocity;
-            double new_angle;
-            
-            while(posSearchContinues)
-            {
+    bool notConverged = true;
+
+    while(notConverged){
+
+        notConverged = false;
+        // Assume that the next iteration will result in convergence. If there is a 
+        // significant difference between the previous and current values in any cell,
+        // then we know we have not reached convergence and set it back to true.
+
+        for (int i = 0; i < 2000; i++){
+            for (int j = 0; j < 2000; j++){
+                
+                // Calculate the optimal torque for the current state.
+                double new_angular_velocity;
+                double new_angle;
+                double final_torque;
+
                 double torque = 1;
                 // Doing conversions from index to angle and angular velocity within the function call
                 // because I AM LAZY
@@ -45,6 +60,10 @@ int bellman_update(std::vector<std::vector<GridCell>> state_mat){
 
                 int angular_velocity_index_pos_t = (new_angular_velocity + 8) * (999.5/8);
                 
+                if(angle_index_pos_t == 0 && angular_velocity_index_pos_t == 1000){
+                    int sdfsdf = 0;
+                }
+
                 constrain(angle_index_pos_t, 0, 1999);
                 constrain(angular_velocity_index_pos_t, 0, 1999);
 
@@ -56,46 +75,81 @@ int bellman_update(std::vector<std::vector<GridCell>> state_mat){
 
                 int angular_velocity_index_neg_t = (new_angular_velocity + 8) * (999.5/8);
 
+                if(angle_index_neg_t == 0 && angular_velocity_index_neg_t == 1000){
+                    int sdfsdf = 0;
+                }
+
                 constrain(angle_index_neg_t, 0, 1999);
                 constrain(angular_velocity_index_neg_t, 0, 1999);
 
                 double max_value = 0;
+                int max_value_angle_index = 0;
+                int max_value_angular_velocity_index = 0;
 
-                for(int k = angle_index_neg_t; k < angle_index_pos_t; k++){
-                    for(int l = angular_velocity_index_neg_t; l < angular_velocity_index_pos_t; l++){
+                for(int k = angle_index_neg_t; k <= angle_index_pos_t; k++){
+                    for(int l = angular_velocity_index_neg_t; l <= angular_velocity_index_pos_t; l++){
                         
                         // We know which states we can "reach" from the current state with the minimum and 
                         // maximum torque. So, we make a bounding box that is determined by the states we can reach
                         // and figure out which of those states is the state with the highest value that we are 
                         // also capable of reaching from our current state.
                         
+                        if (state_mat[k][l].value > max_value){
 
+                            double final_angular_velocity = (l * (8/999.5)) - 8;
+                            double final_angle = (k * (M_PI/999.5)) - M_PI;
 
+                            double angular_velocity = (j * (8/999.5)) - 8;
+                            double angle = (i * (M_PI/999.5)) - M_PI;
+                            
+                            // The angle after the state update is not affected by the torque applied at the 
+                            // current timestep, so it is simple to see if the candidate angle state* after 
+                            // the state update is a feasible next state.
+                            // * there has to be a better way to say that
+                            
+                            if(final_angle - ((angular_velocity * DT) + angle) < M_PI/2000)
+                            {
+                                //                      angle                     angular_velocity
+                                inverseStateTransition((i * (M_PI/999.5)) - M_PI, (j * (8/999.5)) - 8, torque, final_angle, final_angular_velocity);
+
+                                if(torque > -1 && torque < 1){
+                                    final_torque = torque;
+                                    max_value = state_mat[k][l].value;
+                                    max_value_angle_index = k;
+                                    max_value_angular_velocity_index = l;
+                                }  
+
+                            }
+                        }
                     }
                 }
 
-                if(angle_index_pos_t == i && new_angular_velocity == j)
-                {
-                    if(torque + 0.01 <= 1.0)
-                    {
-                        // Increment the torque gets us nowhere.
-                        break;
+                // We found the maximum value that we are capable of reaching from our current state, so now we 
+                // set the value of the current state with a discount factor multiplied by that maximum state.
+                double old_value = state_mat[i][j].value;
+                
+                if(max_value_angle_index != i){
+                   
+                    state_mat[i][j].value = 0.9 * max_value;
+                    state_mat[i][j].optimal_torque = final_torque;
+    
+                    if(old_value - state_mat[i][j].value > 0.01){
+                        notConverged = true;
                     }
-                    torque += 0.01;
-                    continue;                    
+
                 }
 
-                // Calculate the value of the new state
+
             }
         }
     }
-
     return 1;
 }
 
-void inverseStateTransition(double angle, double angular_velocity, double &torque, double new_angle, double new_angular_velocity)
-{
-    double dt = 0.0001;
+
+// Given an input and output angle and angular velocity, calculate the torque it would take to reach that output state.
+void inverseStateTransition(double angle, double angular_velocity, double &torque, double new_angle, double new_angular_velocity){
+    double dt = DT;
     double friction = 0.3;
     double length = 0.5;
                               
@@ -108,7 +162,7 @@ void inverseStateTransition(double angle, double angular_velocity, double &torqu
 void stateTransition(double angle, double angular_velocity, double torque, double &new_angle, double &new_angular_velocity){
     
     double friction = 0.3;
-    double dt = 0.0001;
+    double dt = DT;
     double length = 0.5;                                
     
     new_angular_velocity = angular_velocity - (9.81 * std::sin(angle) * dt)/0.5 - dt * friction * angular_velocity
